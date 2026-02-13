dd321d6074b4b65019a281ea54779286650bb54a
jose <josecschalil@gmail.com>
2026-02-13 13:17:45 +0530

    cleaned temp files from git

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
